eaf44cf1cb898f2906048d42ca8a1739aff5c8b9
Golara Azar <101079632+Golara-Azar@users.noreply.github.com>
2025-01-28 20:05:15 -0800

    Create Serialize and Deserialize Binary Tree.py

---
+++ Medium/Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,92 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res = ""
+        if root is None:
+            return "#"
+        res+=str(root.val)
+        res+=','
+        res+=self.serialize(root.left)
+        res+=','
+        res+=self.serialize(root.right)
+        
+        return res
+        
+
+    def deserialize(self, data_str):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+        if len(data_str)<=1:
+            return None
+        data = data_str.split(",")
+        
+        dummy = tree = TreeNode(eval(data[0]))
+        prev = [tree]
+        i = 1
+        while i<len(data):
+            
+            while data[i]!='#':
+                prev.append(tree)
+                
+                
+            
+                
+                tree.left = TreeNode(eval(data[i]))
+                tree = tree.left
+                
+                i+=1
+                if i==len(data):
+                    
+                    return dummy
+                
+            prev.append(tree)   
+            while data[i]=='#':
+                if len(prev)>0:
+                    tree = prev.pop()
+                    
+                i+=1
+                if i==len(data):
+                    
+                    return dummy
+             
+            
+            tree.right = TreeNode(eval(data[i]))
+            
+            tree = tree.right
+            
+            i+=1
+            if i==len(data):
+                    print(dummy)
+                    return dummy
+            
+            
+        
+        return dummy
+            
+            
+            
+            
+            
+            
+                
+            
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
